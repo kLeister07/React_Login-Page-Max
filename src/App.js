04465abcb31c5ccbd163014223ed86e41d1a294e
Kevin Leister <kevinleister07@gmail.com>
2022-09-23 15:57:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-
+import AuthContext from './store/auth-context';
 import Login from './components/Login/Login';
 import Home from './components/Home/Home';
 import MainHeader from './components/MainHeader/MainHeader';
@@ -27,13 +27,16 @@ function App() {
   };
 
   return (
-    <React.Fragment>
-      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </React.Fragment>
+      <AuthContext.Provider
+          value={{
+              isLoggedIn: isLoggedIn,
+          }}>
+          <MainHeader onLogout={logoutHandler} />
+          <main>
+              {!isLoggedIn && <Login onLogin={loginHandler} />}
+              {isLoggedIn && <Home onLogout={logoutHandler} />}
+          </main>
+      </AuthContext.Provider>
   );
 }
 
